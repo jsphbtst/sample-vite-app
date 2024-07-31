e2307d5be9962c16ef42b9c400498448130661ba
Joseph J. Bautista <joseph.bautista@proton.me>
2024-07-31 08:28:04 +0200

    chore: .env setup and demo

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const isDarkModeDefaultEnabled = import.meta.env.VITE_DARK_MODE_DEFAULT === 'true'
+
   return (
     <div>
       <LogoImages />
       <h1>Lord of Computer Science</h1>
+      { isDarkModeDefaultEnabled ? <p>DARK MODE IS ENABLED</p> : <p>GO BLIND</p>}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
